ba3574d73ca955be35f09d3f4890e81df0f696d2
Jacob Arvidsson <jacobarvidsson@me.com>
2019-07-30 14:43:15 +0100

    Exports default values

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { defaultDomains, defaultSecondLevelDomains, defaultTopLevelDomains } from "./config";
+
+const defaultValues = {
+  domains: defaultDomains,
+  secondLevelDomains: defaultSecondLevelDomains,
+  topLevelDomains: defaultTopLevelDomains,
+};
+
 export { suggest } from "./suggest";
 export { validate } from "./validate";
+export { defaultValues };
